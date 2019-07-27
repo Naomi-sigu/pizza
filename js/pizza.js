74d89f1efc576549686d444295b2fd0261651da2
Naomi <siguneema@gmail.com>
2019-07-27 12:10:28 +0300

    add images and divisions

---
+++ js/pizza.js
@@ -0,0 +1,7 @@
+formData = {
+  'name'     : $('input[name=name]').val(),
+  'email'    : $('input[name=email]').val(),
+  'subject'  : $('input[name=subject]').val(),
+  'message'  : $('textarea[name=message]').val(),
+  'cmethod'  : $('input:radio[name=cmethod]:checked').val()
+};
